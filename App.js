c2d8efd158323b081397e5826a13956ec1561ea4
Cristian Buitrago <cbo@misena.edu.co>
2022-09-05 06:43:00 -0500

    commit final

--- App.js
+++ App.js
@@ -1,13 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      age:{
+        years: 0,
+        month: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      }
+    }
+  }
+
+  getAge(dob){
+    var diff_ms = Date.now() - dob.getTime()
+    var age_df = new Date(diff_ms)
+
+    this.setState({
+      age:{
+        years: age_df.getUTCFullYear() - 1970,
+        months: age_df.getUTCMonth(),
+        days: age_df.getUTCDate()-1,
+        hours: age_df.getUTCHours(),  
+        minutes: age_df.getUTCMinutes(),
+        seconds: age_df.getUTCSeconds(),
+      }
+    })
+  }
+
+  componentDidMount(){
+    this.tick = setInterval(() =>{
+      this.getAge(new Date("August 6, 1979 03:32 UTC+2"));
+    },1000)
+    
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.tick)
+  }
+
+  render (){
+    const { years , month, days, hours, minutes, seconds } = this.state.age
+    return (
+      <View style={styles.container}>
+            <Text>My real Age is</Text>
+            <Text>{years}years </Text>
+            <Text>{month} month </Text>
+            <Text>{days}  days</Text>
+            <Text>{hours} hours </Text>
+            <Text>{minutes}  minutes</Text>
+            <Text>{seconds}  seconds</Text>
+            <StatusBar hidden />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +71,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
